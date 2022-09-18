048a0b2314267d640051247cafb3e68ff1210660
Juan Diaz Guerrero <juanluisdiazguerrero@gmail.com>
2022-09-17 19:36:05 -0500

    feat: add admin panel

--- backend/API/admin.py
+++ backend/API/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import user, product, order
 
 # Register your models here.
+admin.site.register(user)
+admin.site.register(product)
+admin.site.register(order)
