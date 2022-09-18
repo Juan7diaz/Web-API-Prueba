4b4c38623a713552e5343e29119ba60e6e773a95
Juan Diaz Guerrero <juanluisdiazguerrero@gmail.com>
2022-09-18 13:08:32 -0500

    feat: consumes api data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { getUsers, createUser } from "./services/index";
+
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loadUser() {
+      const response = await getUsers();
+      setUsers(response);
+    }
+    loadUser();
+  }, [users]);
+
+  const subir = () => {
+    const name = "jessus";
+    const address = "bogota";
+    const phoneNumber = 1234;
+    createUser({ name, address, phoneNumber });
+  };
+
   return (
-    <div>
-      <h1>Vite + React</h1>
-    </div>
-  )
+    <>
+      {users.map((user) => (
+        <div key={user.id}>
+          {user.name} - {user.address}
+        </div>
+      ))}
+      <button onClick={subir}>subir usuario</button>
+    </>
+  );
 }
 
-export default App
+export default App;
