fc514af001c3faa0915446cf5c916155784d81ee
Juan Diaz Guerrero <juanluisdiazguerrero@gmail.com>
2022-09-23 20:55:48 -0500

    feat: refactoring code

--- frontend/src/components/Drawer/DrawerProduct.jsx
+++ frontend/src/components/Drawer/DrawerProduct.jsx
@@ -1,29 +1,27 @@
-import { useRef } from 'react'
 import { Stack, Box, FormLabel, Input } from '@chakra-ui/react'
 import { ModalLayout } from '../../layout/ModalLayout'
 import { useForm } from '../../hooks/useForm'
 import { createProduct } from '../../services/products'
 import { useNavigate } from 'react-router-dom'
+import { validateProduct } from '../../helpers/ValidateProduct'
 
 export const DrawerProduct = () => {
-  const MAXIMUM_SIZE_ALLOWED = 20
-  const DIGITS_MAXIMUM = 8
-  const firstField = useRef()
-
   const navigate = useNavigate()
 
   const { onInputChange, name, price, code } = useForm({
-    name: 'bolso juan veles',
+    name: '',
     price: '',
     code: '',
   })
 
   const handleSubmit = () => {
-    const isNameValid = name.length <= MAXIMUM_SIZE_ALLOWED && name.length > 0
-    const isCodeValid = code.length <= DIGITS_MAXIMUM && code.length > 0 && !isNaN(code)
-    if (isNameValid && isCodeValid) {
-      console.log(createProduct({ name, code, price }).name)
+    const errors = validateProduct({ name, price, code })
+    const hasErros = Object.entries(errors).length === 0
+    if (hasErros) {
+      createProduct({ name, code, price })
       navigate('/') // solución temporal para que cargue el nuevo producto
+    } else {
+      console.error(errors)
     }
   }
 
@@ -34,7 +32,6 @@ export const DrawerProduct = () => {
           <Box>
             <FormLabel>Product name</FormLabel>
             <Input
-              ref={firstField}
               placeholder='enter the product name'
               name='name'
               value={name}
@@ -44,7 +41,6 @@ export const DrawerProduct = () => {
           <Box>
             <FormLabel>Product code</FormLabel>
             <Input
-              ref={firstField}
               placeholder='enter the product code'
               name='code'
               value={code}
---
+++ frontend/src/helpers/validateProduct.js
@@ -0,0 +1,24 @@
+export const validateProduct = ({ name, price, code }) => {
+  const MAXIMUM_SIZE_ALLOWED = 20
+  const DIGITS_MAXIMUM = 8
+
+  const errors = {}
+
+  const isNameValid = name.length <= MAXIMUM_SIZE_ALLOWED && name.length > 0
+  const isCodeValid = code.length <= DIGITS_MAXIMUM && code.length > 0 && !isNaN(code)
+  const isPriceValid = price > 0
+
+  if (!isNameValid) {
+    errors.name = 'Name must be less than 20 characters'
+  }
+
+  if (!isCodeValid) {
+    errors.code = 'Code must be less than 8 digits'
+  }
+
+  if (!isPriceValid) {
+    errors.price = 'Price must be greater than 0'
+  }
+
+  return errors
+}
